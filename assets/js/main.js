bebeb8a31b691c58574c01b903314749a9b3039f
xNothing <iRestrunt@gmail.com>
2021-10-27 22:02:29 +0300

    edit some settings

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// Navbar
 // Add hoverd class to website sections
 let home = document.querySelector('.home');
 let sections = document.querySelector('.sections');
@@ -10,47 +11,87 @@ let body = document.querySelector('.body');
 let navContainer = document.querySelector('.navContainer');
 let mainContainer = document.querySelector('.mainContainer');
 let topBar = document.querySelector('.topBar');
-
 home.onclick = function putHomeClicked() {
-    // sections.classList.remove('clicked');
-    // lastNews.classList.remove('clicked');
-    // contact.classList.remove('clicked');
-    // home.classList.add('clicked');
+    if (window.innerWidth < 480) {
+        sections.classList.remove('clicked');
+        lastNews.classList.remove('clicked');
+        contact.classList.remove('clicked');
+        home.classList.add('clicked');
+    }
     menuMobile.classList.toggle('activate');
     navContainer.classList.toggle('activate');
     mainContainer.classList.toggle('activate');
     topBar.classList.toggle('activate')
 };
 sections.onclick = function putSectionsClicked() {
-    // home.classList.remove('clicked');
-    // lastNews.classList.remove('clicked');
-    // contact.classList.remove('clicked');
-    // sections.classList.add('clicked');
+    if (window.innerWidth < 480) {
+        home.classList.remove('clicked');
+        lastNews.classList.remove('clicked');
+        contact.classList.remove('clicked');
+        sections.classList.add('clicked');
+    }
     menuMobile.classList.toggle('activate');
     navContainer.classList.toggle('activate');
     mainContainer.classList.toggle('activate');
     topBar.classList.toggle('activate')
 };
 lastNews.onclick = function () {
-    // home.classList.remove('clicked');
-    // sections.classList.remove('clicked');
-    // contact.classList.remove('clicked');
-    // lastNews.classList.add('clicked');
+    if (window.innerWidth < 480) {
+        home.classList.remove('clicked');
+        sections.classList.remove('clicked');
+        contact.classList.remove('clicked');
+        lastNews.classList.add('clicked');
+    }
     menuMobile.classList.toggle('activate');
     navContainer.classList.toggle('activate');
     mainContainer.classList.toggle('activate');
     topBar.classList.toggle('activate')
 };
 contact.onclick = function () {
-    // home.classList.remove('clicked');
-    // sections.classList.remove('clicked');
-    // lastNews.classList.remove('clicked');
-    // contact.classList.add('clicked');
+    if (window.innerWidth < 480) {
+        home.classList.remove('clicked');
+        sections.classList.remove('clicked');
+        lastNews.classList.remove('clicked');
+        contact.classList.add('clicked');
+    }
     menuMobile.classList.toggle('activate');
     navContainer.classList.toggle('activate');
     mainContainer.classList.toggle('activate');
     topBar.classList.toggle('activate')
 };
+menuMobile.onclick = function () {
+    menuMobile.classList.toggle('activate');
+    navContainer.classList.toggle('activate');
+    mainContainer.classList.toggle('activate');
+    topBar.classList.toggle('activate')
+}
+window.addEventListener('scroll', function () {
+    if (window.innerWidth > 480) {
+        if (scrollY < 900) {
+            sections.classList.remove('clicked');
+            lastNews.classList.remove('clicked');
+            contact.classList.remove('clicked');
+            home.classList.add('clicked');
+        } else if (scrollY < 1900) {
+            home.classList.remove('clicked');
+            lastNews.classList.remove('clicked');
+            contact.classList.remove('clicked');
+            sections.classList.add('clicked');
+        } else if (scrollY < 3000) {
+            home.classList.remove('clicked');
+            sections.classList.remove('clicked');
+            contact.classList.remove('clicked');
+            lastNews.classList.add('clicked');
+        } else {
+            home.classList.remove('clicked');
+            sections.classList.remove('clicked');
+            lastNews.classList.remove('clicked');
+            contact.classList.add('clicked');
+        }
+    }
+})
+
+// Dark Mood
 light.onclick = function () {
     dark.classList.remove('clicked');
     light.classList.add('clicked');
@@ -71,32 +112,3 @@ dark.onclick = function () {
     navContainer.classList.add('dark');
     mainContainer.classList.add('dark');
 };
-menuMobile.onclick = function () {
-    menuMobile.classList.toggle('activate');
-    navContainer.classList.toggle('activate');
-    mainContainer.classList.toggle('activate');
-    topBar.classList.toggle('activate')
-}
-window.addEventListener('scroll', function () {
-    if (scrollY < 1041) {
-        sections.classList.remove('clicked');
-        lastNews.classList.remove('clicked');
-        contact.classList.remove('clicked');
-        home.classList.add('clicked');
-    } else if (scrollY < 2083) {
-        home.classList.remove('clicked');
-        lastNews.classList.remove('clicked');
-        contact.classList.remove('clicked');
-        sections.classList.add('clicked');
-    } else if (scrollY < 3124) {
-        home.classList.remove('clicked');
-        sections.classList.remove('clicked');
-        contact.classList.remove('clicked');
-        lastNews.classList.add('clicked');
-    } else {
-        home.classList.remove('clicked');
-        sections.classList.remove('clicked');
-        lastNews.classList.remove('clicked');
-        contact.classList.add('clicked');
-    }
-})
